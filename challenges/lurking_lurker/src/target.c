d4054a906c37ffb1acda6a978e2df25b6ff3f194
njasi <nick@jasinski3.com>
2025-04-26 14:32:10 -0600

    remove extra do work

--- challenges/lurking_lurker/src/target.c
+++ challenges/lurking_lurker/src/target.c
@@ -21,46 +21,6 @@ void handle_signal(int sig)
     }
 }
 
-/**
- * fake job to make the worker threads look busy
- */
-void do_work()
-{
-    size_t current_blocks = 0;
-    void *blocks[MAX_BLOCKS];
-
-    while (1)
-    {
-        // do some busy work
-        int total = 0;
-        for (int i = 0; i < 1000; i++)
-        {
-            total += rand() % 100;
-        }
-
-        // allocate / free randomly to look real
-        if (rand() % 2 == 0 && current_blocks < MAX_BLOCKS)
-        {
-            size_t size = (rand() % (100 - 4) + 4) * 1024;
-            blocks[current_blocks] = malloc(size);
-
-            if (blocks[current_blocks])
-            {
-                current_blocks++;
-            }
-        }
-        else if (current_blocks > 0)
-        {
-            size_t idx = rand() % current_blocks;
-            free(blocks[idx]);
-            blocks[idx] = blocks[current_blocks - 1];
-            current_blocks--;
-        }
-
-        usleep(100000);
-    }
-}
-
 int main(int argc, char *argv[])
 {
     char fake_name[64];
