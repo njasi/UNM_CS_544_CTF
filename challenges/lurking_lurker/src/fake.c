eba48e954f3123d65875e00c300cdfc5aaf68ba2
njasi <nick@jasinski3.com>
2025-04-26 11:01:49 -0600

    add work to fake threads

--- challenges/lurking_lurker/src/fake.c
+++ challenges/lurking_lurker/src/fake.c
@@ -7,29 +7,23 @@
 #include <sys/prctl.h>
 
 void generate_fake_name(char *buf, size_t len) {
-    srand(time(NULL) ^ getpid()); // random seed per PID
-    int type = rand() % 5;
     int cpu = rand() % 16;
     int id = rand() % 100;
 
-    switch (type) {
-        case 0:
-            snprintf(buf, len, "kworker/%d:%d", cpu, id);
-            break;
-        case 1:
-            snprintf(buf, len, "migration/%d", cpu);
-            break;
-        case 2:
-            snprintf(buf, len, "watchdog/%d", cpu);
-            break;
-        case 3:
-            snprintf(buf, len, "ksoftirqd/%d", cpu);
-            break;
-        case 4:
-            snprintf(buf, len, "flush-%d:%d", rand() % 10, rand() % 256);
-            break;
-        default:
-            strncpy(buf, "khelper", len);
+    snprintf(buf, len, "kworker/%d:%d", cpu, id);
+}
+
+/**
+ * fake job to make the worker threads look busy
+ */
+void do_work(){
+    while (1) {
+        int total = 0;
+        for (int i = 0; i < 1000; i++) {
+            total += rand() % 100;
+        }
+
+        sleep(1);
     }
 }
 
@@ -42,7 +36,7 @@ int main(int argc, char *argv[])
 
     memset(argv[0], 0, strlen(argv[0]));
     strncpy(argv[0], fake_name, strlen(fake_name));
-    
+
     while (1) {
         sleep(60);
     }
