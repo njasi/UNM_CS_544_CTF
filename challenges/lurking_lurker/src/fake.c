2ad673a60c36d3ac43867a30aa768c2c95db5358
njasi <nick@jasinski3.com>
2025-04-26 12:17:48 -0600

    change lib stuf

--- challenges/lurking_lurker/src/fake.c
+++ challenges/lurking_lurker/src/fake.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
 #include <unistd.h>
 #include <string.h>
 #include <time.h>
 #include <sys/prctl.h>
+
 
 #define MAX_BLOCKS 100
 
--- challenges/lurking_lurker/src/real.c
+++ challenges/lurking_lurker/src/real.c
@@ -1,38 +1,14 @@
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <signal.h>
+#include <unistd.h>
 #include <string.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
+#include <time.h>
 #include <sys/prctl.h>
+// note that we don't need all these libs, but we
+// include them so we look the same as the other workers
 
-#define SHM_KEY 12345
-#define TARGET_SHR_SIZE 1280
-/**
- * make an allocate shared memory method
- * so we can look the same as the other workers
- */
-
-void *allocate_shared_memory(size_t size)
-{
-    int shmid = shmget(SHM_KEY, size, IPC_CREAT | 0666); // Create shared memory segment
-    if (shmid == -1)
-    {
-        perror("shmget failed");
-        exit(1);
-    }
-
-    void *shm_ptr = shmat(shmid, NULL, 0); // Attach the shared memory
-    if (shm_ptr == (void *)-1)
-    {
-        perror("shmat failed");
-        exit(1);
-    }
-
-    return shm_ptr;
-}
 
 void handle_signal(int sig)
 {
@@ -67,11 +43,6 @@ int main(int argc, char *argv[])
     prctl(PR_SET_NAME, "kworker/0:1");
     memset(argv[0], 0, strlen(argv[0]));
     strcpy(argv[0], "kworker/0:1");
-
-    // allocate shr memory to look like the other workers
-    size_t shm_size = TARGET_SHR_SIZE * 1024;
-    void *shm_ptr = allocate_shared_memory(shm_size);
-    memset(shm_ptr, 0, shm_size);
 
     // signal handler
     signal(SIGTERM, handle_signal);
