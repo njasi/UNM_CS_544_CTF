ae9f73ad5c1e4a77ae8d22d03790081a00f0888e
njasi <nick@jasinski3.com>
2025-05-02 13:05:26 -0600

    suid bit change

--- challenges/path_less_traveled/src/rotate-logs.sh
+++ challenges/path_less_traveled/src/rotate-logs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+echo "logrotate starting\n" >> /home/*/log_status.log
 
-
-logutil rotate /var/log/syslog
+/usr/bin/logrotate
 
 NOW=$( date '+%F_%H:%M:%S' )
-echo "logutil has run successfully @ $NOW" >> /home/*/log_status.log
+echo "logrotate has run successfully @ $NOW\n" >> /home/*/log_status.log
