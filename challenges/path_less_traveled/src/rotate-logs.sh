ad4afc3694d4ba8f752ff060735bf0681e3323c0
njasi <nick@jasinski3.com>
2025-05-02 13:09:40 -0600

    fix logging paths

--- challenges/path_less_traveled/src/rotate-logs.sh
+++ challenges/path_less_traveled/src/rotate-logs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo "logrotate starting\n" >> /home/*/log_status.log
+echo "logrotate starting\n" >> /home/ctf/log_status.log
 
 /usr/bin/logrotate
 
 NOW=$( date '+%F_%H:%M:%S' )
-echo "logrotate has run successfully @ $NOW\n" >> /home/*/log_status.log
+echo "logrotate has run successfully @ $NOW\n" >> /home/ctf/log_status.log
